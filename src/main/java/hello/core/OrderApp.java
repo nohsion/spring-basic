c1d4b812b5cd59d89ca2d68e83766982c9ac894c
sion <68843443+nohsion@users.noreply.github.com>
2022-12-29 00:48:29 +0900

    순수 자바 코드를 이용한 DI

---
+++ src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,29 @@
+package hello.core;
+
+import hello.core.config.AppConfig;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class OrderApp {
+
+    public static void main(String[] args) {
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
+
+        long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        String itemName = "itemA";
+        int itemPrice = 10000;
+        Order order = orderService.createOrder(memberId, itemName, itemPrice);
+
+        System.out.println("order = " + order);
+    }
+}
