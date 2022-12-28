222be2ed2b18fea9caf14e001f4461de2dcaf3b7
sion <68843443+nohsion@users.noreply.github.com>
2022-12-29 01:15:08 +0900

    컴포넌트 스캔과 의존관계 주입 autowired

---
+++ src/main/java/hello/core/config/AutoAppConfig.java
@@ -0,0 +1,14 @@
+package hello.core.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        basePackages = "hello.core",
+        excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+}
---
+++ src/main/java/hello/core/config/AutoAppConfigTest.java
@@ -0,0 +1,20 @@
+package hello.core.config;
+
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AutoAppConfigTest {
+
+    @Test
+    void basicScan() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+        MemberService memberService = ac.getBean(MemberService.class);
+        Assertions.assertInstanceOf(MemberService.class, memberService);
+    }
+
+}
